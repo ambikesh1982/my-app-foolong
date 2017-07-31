d835acbea46da8fa516c5e8e53e305b898d9d6fc
ambikesh1982 <ambikesh1982@gmail.com>
2017-08-01 00:19:53 +0800

    md-fab changes

--- src/app/shared/ui-components/search-icon.component.ts
+++ src/app/shared/ui-components/search-icon.component.ts
@@ -3,10 +3,9 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-search-icon',
   template: `
-    <a md-icon-button 
-        routerLink="/search" routerLinkActive="active" 
-        mdTooltip="Search" mdTooltipShowDelay="1000">
-        <md-icon>search</md-icon>
+    <a md-fab
+      routerLink="/search" routerLinkActive="active">
+      <md-icon>search</md-icon>
     </a>
   `,
   styles: []
